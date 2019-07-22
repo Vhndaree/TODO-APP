681dccdf5605937db70994debf1e9e5c19b3298b
Ram Bhandari <ramjdbhandari51@gmail.com>
2019-07-22 17:26:08 +0545

    Updates

---
+++ precache-manifest.0864d02da7e66cfff649b371dda71f9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba2af42fc4c696c5ceb3032c67585ec",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "be8329a71f051b748afa",
+    "url": "/todo-app/static/css/main.a1ce2435.chunk.css"
+  },
+  {
+    "revision": "471263e36fe8c73db02e",
+    "url": "/todo-app/static/js/2.881f9530.chunk.js"
+  },
+  {
+    "revision": "be8329a71f051b748afa",
+    "url": "/todo-app/static/js/main.da790d9d.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
